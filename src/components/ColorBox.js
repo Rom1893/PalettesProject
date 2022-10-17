dbf8d8717b4dcb3a4ea3942bdfa11d6b9736418b
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-17 14:34:19 -0300

    Added ColorBox component

---
+++ src/components/ColorBox.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import "../Styles/ColorBox.css"
+
+class ColorBox extends Component {
+
+
+    render() {
+
+        return (
+            <div style={{background: this.props.background}} className='ColorBox'>
+                <span>{this.props.name}</span>
+                <span>MORE</span>
+                
+            </div>
+        )
+    }
+}
+
+export default ColorBox;
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import ColorBox from './ColorBox';
+import "../Styles/Palette.css"
 
 class Palette extends Component {
 
 
     render() {
 
+        const colorBoxes = this.props.colors.map(m=>(
+            <ColorBox background ={m.color} name={m.name}/>
+        ))
+
         return (
             <div className='Palette'>
                 <div className='Palette-colors'>
-
+                {colorBoxes}
                 </div>
             </div>
         )
