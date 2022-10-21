70842a86f1abbda702cd4bfe192de6cc4bcb06c7
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-20 22:07:49 -0300

    Snackbar and styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,27 @@ import Slider, { Range } from 'rc-slider';
 import "rc-slider/assets/index.css"
 import "../Styles/NavBar.css"
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
 class NavBar extends Component {
 
-    state={
-        format: "hex"
+    state = {
+        format: "hex",
+        open: false
     }
 
     changeFormat = (e) => {
-        this.setState({format: e.target.value})
+        this.setState({ format: e.target.value, open: true })
         this.props.changeFormat(e.target.value)
+    }
+
+    closeSnackbar = () => {
+        this.setState({
+            open: false
+        })
     }
 
     render() {
@@ -43,6 +51,27 @@ class NavBar extends Component {
                         <MenuItem value="rgba">Rgba - (255, 255, 255, 1.0)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Format Changed to {this.state.format.toUpperCase()}</span>}
+                    ContentProps={{
+                        "aria-describeby": "message-id"
+                    }}
+                    onClose={this.closeSnackbar}
+                    action={[
+                        <IconButton onClick={this.closeSnackbar} 
+                        color="inherit"
+                        key="close"
+                        aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+
+                
+                />
             </header>
         )
     }
