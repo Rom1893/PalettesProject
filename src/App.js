3b2c7426f0355206ad38c4f4c40396b31b1a7b6b
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-23 21:45:59 -0300

    Palette routes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ import {Route, Switch} from "react-router-dom"
 
 class App extends Component {
 
+  findPatelle = (id) => {
+    return SeedColors.find(function(palette){
+      return palette.id === id
+    })
+  }
+
   render() {
  
 
     return (
         <Switch>
           <Route exact path="/" render={()=><h1>PALETTE LIST GOES HERE</h1>}/>
-          <Route exact path="/palette/:id" render={()=><h1>WAN PALETTE</h1>}/>
+          <Route 
+          exact 
+          path="/palette/:id" 
+          render={(routeProps)=> 
+          <Palette 
+          palette ={generatePalette(this.findPatelle(routeProps.match.params.id))}/> }
+          />
         </Switch>
 
       // <div className=''>
