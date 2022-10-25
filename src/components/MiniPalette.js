75142cb2133f1311764d7b8a3ba11536b3aa5c4c
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-25 14:22:58 -0300

    Added miniColorBoxes

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -6,12 +6,18 @@ import { withStyles } from "@material-ui/styles";
 
 function MiniPalette(props) {
 
-
-    const { classes, paletteName, emoji } = props;
+    const { classes, paletteName, emoji, colors } = props;
+    const miniColorBoxes = colors.map(m => (
+        <div
+          className={classes.miniColor}
+          style={{ backgroundColor: m.color }}
+          key={m.name}
+        />
+    ))
     return (
         <div className={classes.root}>
             <div className={classes.colors}>
-
+                {miniColorBoxes}
             </div>
             <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
         </div>
