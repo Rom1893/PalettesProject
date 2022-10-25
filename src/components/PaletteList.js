885507b630b5718e977a49527210fae8ff3f92e5
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-25 14:06:18 -0300

    PaletteList Styling

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
+import { withStyles } from "@material-ui/styles";
+import styles from "../Styles/PaletteListStyles";
 
 class PaletteList extends Component {
 
 
 
   render() {
-
+    const { classes, container } = this.props;
 
     return (
-      <div>
-        {this.props.palettes.map(m => (
-          <MiniPalette {...m}/>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>Color Picker</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {this.props.palettes.map(m => (
+              <MiniPalette {...m} />
+            ))}
+          </div>
+        </div>
 
-        ))}
       </div>
     );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
