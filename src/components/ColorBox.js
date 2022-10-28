e7279753f10a59590f829099973be5a1e2a01299
Rodrigo Morán <rodrigobq18@gmail.com>
2022-10-28 15:47:12 -0300

    Dynamic text color

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import "../Styles/ColorBox.css"
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from "react-router-dom"
+import chroma from 'chroma-js';
 
 
 class ColorBox extends Component {
@@ -18,22 +19,24 @@ class ColorBox extends Component {
 
     render() {
         const { name, background, id, paletteId } = this.props
+        const isDarkColor = chroma(background).luminance() <= 0.18
+        const isLightColor = chroma(background).luminance() >= 0.18
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className='ColorBox'>
                     <div style={{ background }} className={`copy-overlay ${this.state.copied && "show"}`} />
                     <div className={`copy-msg ${this.state.copied && "show"}`}>
                         <h1>Copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && "dark-text"}>{background}</p>
                     </div>
                     <div className='copy-container'>
                         <div className='box-content'>
-                            <span>{name}</span>
+                            <span className={isDarkColor && "light-text"}>{name}</span>
                         </div>
-                        <button className='copy-button'>Copy</button>
+                        <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
                     </div>
                     <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-                        <span className='see-more'>More</span>
+                        <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
                     </Link>
                 </div>
             </CopyToClipboard>
@@ -42,6 +45,8 @@ class ColorBox extends Component {
 }
 
 export default ColorBox;
+
+
 
 
 
