f5e227f243057ff89c98b473d600d0ecb67a5111
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-09 00:52:37 -0300

    Fixed random color duplication bug

--- src/App.js
+++ src/App.js
@@ -105,6 +105,18 @@ class App extends Component {
                   </Page>
                 }
               />
+
+              <Route
+                render={(routeProps) =>
+                  <Page>
+                    <PaletteList
+                      palettes={this.state.palettes}
+                      deletePalette={this.deletePalette}
+                      {...routeProps}
+                    />
+                  </Page>
+                }
+              />
             </Switch>
           </CSSTransition>
         </TransitionGroup>
